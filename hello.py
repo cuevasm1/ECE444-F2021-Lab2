b06fdc3f552653d21303a4fe99bd2681b350cf4c
cuevasm1 <janelle.cuevas@mail.utoronto.ca>
2021-09-26 21:46:47 -0400

    began activity 3

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
-import flask
+from flask import Flask, render_template
+from datetime import datetime
 
-from flask import Flask
+# from flask_moment import Moment
+# moment = Moment(app)
+
+###
+from flask_bootstrap import Bootstrap
 app =Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    #return '<h1>Hello World!</h1>'
+    #return render_template('index.html', current_time=datetime.utcnow())
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
